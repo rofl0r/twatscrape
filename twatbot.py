fdbd35cff091f16e46bc49199ecf536dbf8027a7
rofl0r <retnyg@gmx.net>
2018-08-01 05:59:12 +0100

    add missing parens in RT display

--- twatbot.py
+++ twatbot.py
@@ -95,7 +95,7 @@ def htmlize_twat(twat):
 		(twat["user"], twat["id"], twat["user"])
 
 	else:
-		user_str = "<a target='_blank' href='https://twitter.com/%s'>%s</a> (RT <a target='_blank' href='https://twitter.com/%s/status/%s'>%s</a>" % \
+		user_str = "<a target='_blank' href='https://twitter.com/%s'>%s</a> (RT <a target='_blank' href='https://twitter.com/%s/status/%s'>%s</a>)" % \
 		(twat['owner'], twat['owner'], twat['user'], twat['id'], twat['user'])
 		#user_str = "<a target='_blank' href='https://twitter.com/%s/status/%s'>%s</a> (RT <a target='_blank' href='https://twitter.com/%s'>%s</a>)" % \
 		#(twat["user"], twat["id"], twat["user"], twat["owner"], twat["owner"])
