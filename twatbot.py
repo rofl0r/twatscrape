b1a55b5959f564e5eedb847c23c3f22714a2e791
mickael <mickael@serneels.xyz>
2018-09-24 21:38:48 +0200

    introduces load_watchlist() function

--- twatbot.py
+++ twatbot.py
@@ -564,6 +564,8 @@ def start_server(ip, port):
 	t.start()
 	return t, done
 
+def load_watchlist():
+	return [x.rstrip('\n').lower() for x in open(args.watchlist, 'r').readlines() if not x.startswith(';')]
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -609,7 +611,8 @@ if __name__ == '__main__':
 	## global rshttp object used with get_twats()
 	twitter_rshttp = RsHttp('twitter.com', ssl=True, port=443, keep_alive=True, follow_redirects=True, auto_set_cookies=True, proxies=args.proxy, user_agent="curl/7.60.0")
 
-	watchlist = [x.rstrip('\n') for x in open(args.watchlist, 'r').readlines() if not x.startswith(';')]
+	#watchlist = [x.rstrip('\n') for x in open(args.watchlist, 'r').readlines() if not x.startswith(';')]
+	watchlist = load_watchlist()
 	if args.reload > 0: watchlist_ticks = time.time()
 	random.shuffle(watchlist)
 
@@ -628,7 +631,7 @@ if __name__ == '__main__':
 	while True:
 		try:
 			if args.reload > 0 and (time.time() - watchlist_ticks) > args.reload:
-				watchlist = [x.rstrip('\n') for x in open(args.watchlist, 'r').readlines() if not x.startswith(';')]
+				watchlist = load_watchlist()
 				random.shuffle(watchlist)
 				watchlist_ticks = time.time()
 				## load known twats or create empty list
