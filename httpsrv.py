3f61c0a1e8ab7e4b9d5489e556bfa6eccc5714b0
Your Name <you@example.com>
2021-05-18 16:05:12 +0200

    httpsrv: makes it able to listen for 128 clients (hardcoded)

--- httpsrv.py
+++ httpsrv.py
@@ -169,7 +169,7 @@ class HttpSrv():
 		s = socket.socket(af, socket.SOCK_STREAM)
 		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		s.bind((sa[0], sa[1]))
-		s.listen()
+		s.listen(128)
 		self.s = s
 
 	def wait_client(self):
