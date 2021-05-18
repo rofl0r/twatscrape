eabcfba5137d7237d3df81079c0114f3c88d2939
Your Name <you@example.com>
2021-05-18 13:38:22 +0200

    httpsrv: allow to define how many clients to listen for

--- httpsrv.py
+++ httpsrv.py
@@ -159,9 +159,10 @@ class HttpSrv():
 
 		return None, None
 
-	def __init__(self, listenip, port):
+	def __init__(self, listenip, port, clients):
 		self.port = port
 		self.listenip = listenip
+		self.clients = clients
 		self.s = None
 
 	def setup(self):
@@ -169,7 +170,7 @@ class HttpSrv():
 		s = socket.socket(af, socket.SOCK_STREAM)
 		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		s.bind((sa[0], sa[1]))
-		s.listen(1)
+		s.listen(self.clients)
 		self.s = s
 
 	def wait_client(self):
