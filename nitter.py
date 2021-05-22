320558936dd23ea6e2526ddc2c1e2f261819fc8f
Your Name <you@example.com>
2021-05-22 15:22:55 +0200

    introduce nitter_fetch(), code rewrite
    Try to re-use the same nitter_rshttp instance as long as possible.
    This leads to cleaner code, I hope better perfs and less costly operations

--- nitter.py
+++ nitter.py
@@ -16,8 +16,10 @@ def nitter_connect(nitters, proxies):
 	while True:
 		host = get_nitter_instance(nitters)
 		# no available instance
+		# sleep for a while and try again
 		if not host:
-			return False, host, nitters
+			time.sleep(60)
+			continue
 
 		http = RsHttp(host=host, port=443, timeout=15, ssl=True, keep_alive=True, follow_redirects=True, auto_set_cookies=True, proxies=proxies, user_agent="curl/7.60.0")
 		http.set_cookie('hlsPlayback=on')
@@ -25,3 +27,20 @@ def nitter_connect(nitters, proxies):
 			return http, host, nitters
 		else:
 			nitters = set_invalid_nitter(host, nitters, 86400)
+
+def nitter_get(req, http, host, nitters, proxies):
+	while True:
+
+		# initiate connection to random nitter instance, if no
+		# opened http connection exists
+		if not http:
+			http, host, nitters = nitter_connect(nitters, proxies)
+
+		hdr, res = http.get(req)
+		# we hit rate limiting
+		if not  '200 OK' in hdr:
+			nitters = set_invalid_nitter(host, nitters)
+			http = None
+
+		else:
+			return hdr, res, http, host, nitters
